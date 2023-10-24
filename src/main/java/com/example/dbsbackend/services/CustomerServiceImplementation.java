74f89473255ec991feccc448fec7bc4b4a131a73
Vivekanand P V <vivekanandpv@outlook.com>
2023-10-24 17:03:13 +0530

    service implementation

--- src/main/java/com/example/dbsbackend/services/CustomerServiceImplementation.java
+++ src/main/java/com/example/dbsbackend/services/CustomerServiceImplementation.java
@@ -1,12 +1,16 @@
 package com.example.dbsbackend.services;
 
+import com.example.dbsbackend.entities.Customer;
+import com.example.dbsbackend.exceptions.RecordNotFoundException;
 import com.example.dbsbackend.repositories.CustomerRepository;
 import com.example.dbsbackend.viewmodels.CustomerCreateViewModel;
 import com.example.dbsbackend.viewmodels.CustomerUpdateViewModel;
 import com.example.dbsbackend.viewmodels.CustomerViewModel;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomerServiceImplementation implements CustomerService {
@@ -18,26 +22,63 @@ public class CustomerServiceImplementation implements CustomerService {
 
     @Override
     public List<CustomerViewModel> getAll() {
-        return null;
+        return repository.findAll()
+                .stream()
+                .map(c -> toViewModel(c))
+                .collect(Collectors.toList());
     }
 
     @Override
     public CustomerViewModel getById(int id) {
-        return null;
+        return toViewModel(getEntityById(id));
     }
 
     @Override
     public CustomerViewModel create(CustomerCreateViewModel viewModel) {
-        return null;
+        return toViewModel(repository.saveAndFlush(toEntity(viewModel)));
     }
 
     @Override
     public CustomerViewModel update(int id, CustomerUpdateViewModel viewModel) {
-        return null;
+        var entityDb = getEntityById(id);
+        var entityUpdated = toEntity(entityDb, viewModel);
+        repository.saveAndFlush(entityUpdated);
+        return toViewModel(entityUpdated);
+
+//        return toViewModel(repository.saveAndFlush(toEntity(getEntityById(id), viewModel)));
     }
 
     @Override
     public void deleteById(int id) {
+        repository.delete(getEntityById(id));
+    }
 
+    private Customer getEntityById(int id) {
+        return repository.findById(id)
+                .orElseThrow(
+                        () ->
+                                new RecordNotFoundException(
+                                        String.format(
+                                                "Customer with id: %d is not found", id
+                                        )
+                                )
+                );
+    }
+
+    private CustomerViewModel toViewModel(Customer entity) {
+        var viewModel = new CustomerViewModel();
+        BeanUtils.copyProperties(entity, viewModel);
+        return viewModel;
+    }
+
+    private Customer toEntity(CustomerCreateViewModel viewModel) {
+        var entity = new Customer();
+        BeanUtils.copyProperties(viewModel, entity);
+        return entity;
+    }
+
+    private Customer toEntity(Customer entityDb, CustomerUpdateViewModel viewModel) {
+        BeanUtils.copyProperties(viewModel, entityDb);
+        return entityDb;
     }
 }
